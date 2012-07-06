eb5e1de099c7a006e70c93f5075b444087d5c11c
hoshina85 <hoshina85@gmail.com>
2012-07-06 22:36:45 +0900

    fix access control

--- protected/controllers/SeriesController.php
+++ protected/controllers/SeriesController.php
@@ -31,11 +31,11 @@ class SeriesController extends Controller
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('check'),
-				'users'=>array('crifff'),
+				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('create','update','admin','delete'),
-				'users'=>array('crifff'),
+				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('crifff'),
+				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
