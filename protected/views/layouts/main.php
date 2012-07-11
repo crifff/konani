24f218e378d9413e89cf056fc2998995f2437cf5
hoshina85 <hoshina85@gmail.com>
2012-07-11 11:58:13 +0900

    favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/css/bootstrap/css/bootstrap.css" rel="stylesheet">
   <link href="/css/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-  <link rel="shortcut icon" href="assets/ico/favicon.ico">
+  <link rel="shortcut icon" href="/favicon.ico">
   <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
   <script type="text/javascript">
     var _gaq = _gaq || [];
