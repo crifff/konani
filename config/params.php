4059c283289590238082ee01e455994e411eb97b
hoshina85 <hoshina85@gmail.com>
2014-03-31 23:46:37 +0900

    getenv

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'twitterConsumerKey' => isset($_ENV['TWITTER_CONSUMER_KEY']) ? $_ENV['TWITTER_CONSUMER_KEY'] : "",
-    'twitterConsumerSecret' => isset($_ENV['TWITTER_CONSUMER_SECRET']) ? $_ENV['TWITTER_CONSUMER_SECRET'] : "",
-    'bingAccountKey' => isset($_ENV['BING_ACCOUNT_KEY']) ? $_ENV['BING_ACCOUNT_KEY'] : "",
+    'twitterConsumerKey' => getenv('TWITTER_CONSUMER_KEY'),
+    'twitterConsumerSecret' => getenv('TWITTER_CONSUMER_SECRET'),
+    'bingAccountKey' => getenv('BING_ACCOUNT_KEY'),
 ];
